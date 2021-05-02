d197806810ae2c3b4a1b27b91da7d97d0e026481
Starkeyofficial <sajjanmalik6@gmail.com>
2021-05-03 00:03:04 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  // templateUrl: './app.component.html',
+  template: `<div><h1>{{title}}</h1>
+  <pm-products></pm-products>
+  </div>`
+  // styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Acme Product Management';
+}
