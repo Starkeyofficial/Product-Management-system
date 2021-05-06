643f1b52ffa2b9739058340a53407ff18c32b065
Starkeyofficial <sajjanmalik6@gmail.com>
2021-05-06 19:16:58 +0530

    Added Product List fetching service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
 import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -13,7 +14,7 @@ export class ProductListComponent implements OnInit{
     imageWidth : number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    filteredProducts: IProduct[] = [];
+    
 
     private _listFilter: string = '';
 
@@ -26,29 +27,13 @@ export class ProductListComponent implements OnInit{
       this.filteredProducts = this.performFilter(value);
     }
 
-    products: IProduct[] = [
-        {
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2021",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-          "productId": 5,
-          "productName": "Hammer",
-          "productCode": "TBX-0048",
-          "releaseDate": "May 21, 2021",
-          "description": "Curved claw steel hammer",
-          "price": 8.9,
-          "starRating": 4.8,
-          "imageUrl": "assets/images/hammer.png"
-        }
-      ];
+    filteredProducts: IProduct[] = [];
+    products: IProduct[] = [];
 
+
+    constructor(private productService : ProductService){
+
+    }
     
     performFilter(filterBy : string) : IProduct[]{
       filterBy = filterBy.toLocaleLowerCase();
@@ -60,6 +45,8 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void{
+      this.products = this.productService.getProducts();
+      this.filteredProducts = this.products;
       this.listFilter = 'cart';
     }
 
